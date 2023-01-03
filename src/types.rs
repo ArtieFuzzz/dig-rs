3efcfc4c31fa4f4512d8cbaa64598095aafb9013
♥ ArtieFuzzz ♥ <48877462+ArtieFuzzz@users.noreply.github.com>
2023-01-03 16:28:42 +1100

    fix: Fields should be public

--- src/types.rs
+++ src/types.rs
@@ -3,36 +3,36 @@ use serde::{Deserialize, Serialize};
 #[derive(Debug, Serialize, Deserialize)]
 pub struct CloudFlareResponse {
     #[serde(rename = "Status")]
-    status: i64,
+    pub status: i64,
     #[serde(rename = "TC")]
-    tc: bool,
+    pub tc: bool,
     #[serde(rename = "RD")]
-    rd: bool,
+    pub rd: bool,
     #[serde(rename = "RA")]
-    ra: bool,
+    pub ra: bool,
     #[serde(rename = "AD")]
-    ad: bool,
+    pub ad: bool,
     #[serde(rename = "CD")]
-    cd: bool,
+    pub cd: bool,
     #[serde(rename = "Question")]
-    question: Vec<Question>,
+    pub question: Vec<Question>,
     #[serde(rename = "Answer")]
-    answer: Vec<Answer>,
+    pub answer: Vec<Answer>,
 }
 
 #[derive(Debug, Serialize, Deserialize)]
 pub struct Answer {
-    name: String,
+    pub name: String,
     #[serde(rename = "type")]
-    answer_type: i64,
+    pub answer_type: i64,
     #[serde(rename = "TTL")]
-    ttl: i64,
-    data: String,
+    pub ttl: i64,
+    pub data: String,
 }
 
 #[derive(Debug, Serialize, Deserialize)]
 pub struct Question {
-    name: String,
+    pub name: String,
     #[serde(rename = "type")]
-    question_type: i64,
+    pub question_type: i64,
 }
